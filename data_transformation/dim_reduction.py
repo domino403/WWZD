55fb485de3e2c654e1a3e163a157c0a0109d43d4
dgil <dominik.gil@dolby.com>
2024-12-18 16:26:57 +0100

    save work

---
+++ data_transformation/dim_reduction.py
@@ -0,0 +1,9 @@
+from read_parquet import read_parquet, PARQUET_FILE, save_data_batch
+import pandas as pd
+
+
+if __name__ == "__main__":
+    # test_data_path = save_data_batch(read_parquet(PARQUET_FILE), 100)
+    TEST_FILE_PATH = r"C:\Users\dgil\Documents\git-repo\WWZD" \
+                     r"\data_transformation\first_100_rows.parquet"
+    df = read_parquet(TEST_FILE_PATH)
