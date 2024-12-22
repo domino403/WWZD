1b2df3bc49bce646fc5c195b953b4064b1e4c5da
domino403 <gildominik@gmail.com>
2024-12-22 19:34:53 +0100

    prepare data menager

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
-import logging
-from logging.handlers import RotatingFileHandler
+import os
 
-from src.data_transformation.data_loader import data_loader
+from logging_config import setup_logger
+from src.data_transformation.data_menager import data_menager
 
 
-def setup_logger():
-    # initialize logger
-    logger = logging.getLogger("WWZD_main")
-    logger.setLevel(logging.DEBUG)
-
-    # create console handler and set level to debug
-    console_channel = logging.StreamHandler()
-    console_channel.setLevel(logging.WARNING)
-
-    file_handler = RotatingFileHandler(
-        "WWZD.log", mode="w", maxBytes=5 * 1024 * 1024, backupCount=3
-    )
-    file_handler.setLevel(logging.INFO)
-
-    # create formatter
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    console_channel.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-
-    # add console_channel to logger
-    logger.addHandler(console_channel)
-    logger.addHandler(file_handler)
-
-    return logger
+LOGGER = setup_logger()
 
 
 if __name__ == "__main__":
-    logger = setup_logger()
-    logger.debug("debug message")
-    logger.info("info message")
-    logger.warning("warn message")
-    logger.error("error message")
-    logger.critical("critical message")
-    data_loader = data_loader("data")
+    LOGGER.info("Starting main.py!")
+    LOGGER.info(f"Current working directory: '{os.getcwd()}'")
+    data_loader = data_menager("data/output_data.parquet")
+    data_loader.load_parquet()
+    LOGGER.info("Data loading completed.")
+    data_loader.prepare_data()
+    # print(f"{type(data_loader.DataFrame["ILSVRC2012_test_00000001.JPEG"])}")
+    # print(data_loader.prepare_data().head(3))
+    # LOGGER.info("Data loading completed.")
---
+++ src/data_transformation/data_menager.py
@@ -0,0 +1,72 @@
+import os
+import time
+
+import polars as pl
+
+from logging_config import setup_logger
+
+
+LOGGER = setup_logger()
+
+
+class data_menager:
+    def __init__(self, data_path: str):
+        LOGGER.info(f"Data menager initialized, input path: '{data_path}'")
+
+        self.data_path = os.path.abspath(data_path)
+        self.DataFrame = None
+
+    def load_parquet(self):
+        """
+        Reads a Parquet file using the Polars library and measures the time taken.
+        """
+        LOGGER.info(f"Checking if '{self.data_path}' exists")
+        if not os.path.exists(self.data_path):
+            LOGGER.error(f"Path '{self.data_path}' does not exist.")
+            raise FileNotFoundError(f"Path '{self.data_path}' does not exist.")
+
+        try:
+            LOGGER.info(f"Starting to load parquet file from '{self.data_path}'")
+            start_time = time.time()
+            self.DataFrame = pl.read_parquet(self.data_path)
+            LOGGER.info(
+                f"Parquet file loaded successfully. Time taken:  {time.time() - start_time}"
+            )
+
+        except Exception as e:
+            LOGGER.error(f"Error while loading parquet file: {e}", exc_info=True)
+
+    def prepare_data(self):
+        if self.DataFrame is None:
+            LOGGER.error("No data loaded yet.")
+            raise ValueError("No data loaded yet.")
+
+        LOGGER.info("## Preparing data - DataFrame ##")
+        start_time = time.time()
+        self.DataFrame = self.DataFrame.transpose(
+            include_header=True, column_names=["image_params_list"]
+        )
+        self.DataFrame = self.DataFrame.rename({"column": "image_ID"})
+        LOGGER.info(
+            f"Transposed to DataFrame{self.DataFrame.shape} with column names: {self.DataFrame.columns}"
+        )
+
+        self.DataFrame = self.DataFrame.lazy().with_columns(
+            pl.col("image_params_list").list.to_array(1000).alias("image_params_array"),
+        )
+        self.DataFrame = self.DataFrame.lazy().drop("image_params_list")
+        self.DataFrame = (
+            self.DataFrame.lazy()
+            .with_columns(
+                pl.col("image_params_array").arr.to_struct(
+                    fields=[f"param_{x}" for x in range(1000)]
+                ),
+            )
+            .unnest("image_params_array")
+        )
+        LOGGER.info("Etracted image_params_list to separated columns.")
+
+        self.DataFrame = self.DataFrame.collect()
+        LOGGER.info(
+            f"Data preparation completed. Final shape: {self.DataFrame.shape}, Time taken:  {time.time() - start_time}"
+        )
